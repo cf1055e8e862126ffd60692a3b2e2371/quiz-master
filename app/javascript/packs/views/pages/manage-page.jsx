7bba5369f08a1bb74ac3b1c96b869cd1c9b1f973
cf1055e8e862126ffd60692a3b2e2371 <mukoumizunamimizuku@yahoo.co.jp>
2017-08-29 07:26:21 +0900

    Fix ManagePage to separate Questions and add tests

--- app/javascript/packs/views/pages/manage-page.jsx
+++ app/javascript/packs/views/pages/manage-page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import NewQuestion from '../manage/new-question'
-import QuestionRow from '../manage/question-row'
+import Questions from '../manage/questions'
 
 class ManagePage extends React.Component {
   constructor(props) {
@@ -14,23 +14,7 @@ class ManagePage extends React.Component {
     return (
       <div className="manage-page">
         <NewQuestion />
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="col-md-1">Index</th>
-              <th className="col-md-6">Content</th>
-              <th className="col-md-3">Answer</th>
-              <th className="col-md-2" />
-            </tr>
-          </thead>
-          <tbody>
-            {
-              this.props.questions.map((question, i) => (
-                <QuestionRow key={question.id} order={i + 1} question={question} />
-              ))
-            }
-          </tbody>
-        </table>
+        <Questions questions={this.props.questions} />
       </div>
     )
   }
